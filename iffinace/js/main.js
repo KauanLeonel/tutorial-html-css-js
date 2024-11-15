5c343c2faf2d0963217e91ac908652d56f60c04c
Kauan Leonel <kauan.br.char@gmail.com>
2024-11-14 22:20:36 -0300

    Continuando Jscript - 14/11

--- iffinace/js/main.js
+++ iffinace/js/main.js
@@ -14,3 +14,40 @@ function closeModal(event){
     if(event.target.className === 'modal')
         return modal.style.display = 'none'
 }
+
+function handleSubmitAddTicker (event){
+    event.preventDefault()
+    console.log(event.target.urlLogo)
+}
+
+function AddTicker(event){
+    event.preventDefault()
+    const cardList = document.querySelector('#card-list')
+
+    //evenet.target (form)
+    const urlLogo= event.target.urlLogo.value
+    const namecompany = event.target.namecompany.value
+    const ticker = event.target.ticker.value
+    const quantity = event.target.quantity.value
+    const closedValue = event.target.quantity.value
+
+    cardList.innerHTML += `
+                <div class="card-ticker">
+                    <header>
+                        <img src="${urlLogo}" alt="Logo">
+                        <h4>${namecompany}</h4>
+                        <span> ${ticker}</span>
+                    </header>
+                    <main>
+                        <p>Valor: <span style="color: #lebdle">${closedValue} <span>🔺🔻</span></span></p>
+                    </main>
+                    <footer>
+                        <p>Quantidade: <span> ${quantity} </span></p>
+                        <p>Posição: <span>R$: ${quantity * closeModal}</span></p>
+                    </footer>
+                </div>
+           `
+    closeModal()
+    event.target.reset()
+
+}
